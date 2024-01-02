d1605b29f77f615240780f3cc71902be4dde8161
Ale DN <dinucci@protonmail.ch>
2024-01-03 00:57:54 +0100

    Create a1111.sh

---
+++ a1111.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Debian-based:
+sudo apt install wget git python3 python3-venv libgl1 libglib2.0-0
+
+
+# Red Hat-based:
+# sudo dnf install wget git python3 gperftools-libs libglvnd-glx 
+
+# openSUSE-based:
+# sudo zypper install wget git python3 libtcmalloc4 libglvnd
+
+# Arch-based:
+# sudo pacman -S wget git python3
+
+wget -q https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh
+
+./webui.sh --xformers --share
